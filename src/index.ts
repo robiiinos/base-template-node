9fa83336b7ea7eaed9f108e07dd7fd12df09cb0f
robiiinos <dyrda.robin@outlook.fr>
2023-04-11 10:08:12 +0900

    fix: Exit on Error

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,6 @@ const main = async () => {
   logger.info("Hello World!");
 };
 
-main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    logger.error(error);
-    process.exit(1);
-  });
+main().catch((error) => {
+  logger.error(error);
+});
