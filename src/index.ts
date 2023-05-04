a570bb3e9eb0103c283b2ff9d6453a3845e84408
Brykou <11095630+Brykou@users.noreply.github.com>
2023-05-04 20:53:14 +0800

    chore: Cleanup on exit (#3)

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,13 @@ import logger from "./utils/logger";
 
 const main = async () => {
   logger.info("Hello World!");
+
+  const cleanup = () => {
+    logger.info("App is closing");
+  };
+  process.on("exit", cleanup); // app is closing
+  process.on("SIGINT", cleanup); // ctrl+c event
+  process.on("SIGTERM", cleanup); // "kill pid" (like nodemon restart)
 };
 
 main().catch((error) => {
